77ed6c9ae4049ed444340c5f09abe42ae0f72476
Gurgen Torosyan <gurgen.torosyan@exio.tech>
2022-06-06 19:11:49 +0400

    dsds

--- contracts/InvestG.sol
+++ contracts/InvestG.sol
@@ -1,6 +1,7 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+
 import "hardhat/console.sol";
 import "./InvestTokenG.sol";
 
@@ -8,30 +9,25 @@ contract InvestG {
     InvestTokenG investTokenG;
     mapping(address => uint256) public tokenInvestments;
     mapping(address => uint256) public etherInvestments;
-    uint16 countedBlock = 0;
+    
     address owner;
 
-    constructor() {
+    constructor(InvestTokenG _token) {
+        require(address(_token) != address(0), "Zero address is not allowed!");
+        investTokenG = _token;
 		owner = msg.sender;
-    }
-
-	modifier onlyOwner {
-		require(msg.sender == owner, "CoinFlip: Only owner");
-		_;
-	}
+    } 
 
     function invest(uint256 _tokenval ) external payable {
-        countedBlock++;
+        console.log(block.number);
+        
         
         if(msg.value > 0) {
             etherInvestments[msg.sender] = msg.value;
         } else {
             tokenInvestments[msg.sender] = _tokenval;
         }
-        if(countedBlock == 5) {
-            percents();
-            countedBlock = 0; 
-        }
+        
 
     }
 
@@ -40,12 +36,12 @@ contract InvestG {
         etherInvestments[msg.sender] +=  etherInvestments[msg.sender] * 105 / 100 - tokenInvestments[msg.sender];
     }
 
-    function withdrawEth(uint256 _amount) payable external onlyOwner {
+    function withdrawEth(uint256 _amount) external {
 		require(_amount <= address(this).balance, "Error");
 		payable(msg.sender).transfer(_amount);
 	}
 
-    function withdrawToken(uint256 _amount) external onlyOwner {
+    function withdrawToken(uint256 _amount) external {
 		require(_amount <= address(this).balance, "Error");
 		payable(msg.sender).transfer(_amount);
 	}
